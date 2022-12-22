7c74f5edf7980cb139c948217357af2f31460343
T-i-n-o <valentino.heubach@web.de>
2022-12-22 20:47:56 +0100

    Changing the size if the screen in the config file

--- include/EmulatorConfig.h
+++ include/EmulatorConfig.h
@@ -3,7 +3,7 @@
 
 #define WINDOW_TITLE "My FreeRTOS Emulator"
 #define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_HEIGHT 504
 // Relative to bin directory
 #define RESOURCES_DIRECTORY "../resources"
 // Relative to resources directory
