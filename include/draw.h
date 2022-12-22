ccf3f035ca20b1501f985369135d17cee8c51bf5
T-i-n-o <valentino.heubach@web.de>
2022-12-23 00:10:33 +0100

    Adding drawButton to header file

--- include/draw.h
+++ include/draw.h
@@ -6,12 +6,21 @@
 #ifndef DRAW_H
 #define DRAW_H
 
+#include "TUM_Draw.h"
+
 /**
  * @brief Prints the backround image on the screen 
  * @return 1 on success and 0 when failed
  */
 int drawBackround();
 
+
+/**
+ * @brief Prints the backround image on the screen 
+ * @param pos x and y coordinates of the button position
+ * @param name String of the displayed name
+ * @return 1 on success and 0 when failed
+ */
 int drawButton(coord_t pos, char *name);
 
 #endif //__DRAW_H__
