454795cc5f1a6aa7000a53f9332328f7f39b5f03
T-i-n-o <valentino.heubach@web.de>
2022-12-23 01:27:42 +0100

    Adding the drawLogo function to draw.c/.h

--- include/draw.h
+++ include/draw.h
@@ -8,12 +8,17 @@
 
 #include "TUM_Draw.h"
 
+/* Defines for drawButton */
+#define BUTTON_FONT "Minecraft.ttf"
+#define BUTTON_FONT_SIZE 22
+#define BOX_WIDTH 130
+#define BOX_HEIGHT 40
+
 /**
  * @brief Prints the backround image on the screen 
  * @return 1 on success and 0 when failed
  */
 int drawBackround();
-
 
 /**
  * @brief Prints the backround image on the screen 
@@ -22,5 +27,12 @@ int drawBackround();
  * @return 1 on success and 0 when failed
  */
 int drawButton(coord_t pos, char *name);
+
+/**
+ * @brief Prints the Logo on the screen 
+ * @param pos x and y coordinates of the logo position
+ * @return 1 on success and 0 when failed
+ */
+int drawLogo(coord_t pos);
 
 #endif //__DRAW_H__
--- src/draw.c
+++ src/draw.c
@@ -6,17 +6,18 @@
 #include "draw.h"
 
 /* Filenames */
-#define BACKGROUND_FILENAME "flappybird.png"
+#define BACKGROUND_FILENAME "background.png"
+#define LOGO_FILENAME "FlappyBird.png"
 
 /* Definitions for drawButton */
-#define BOX_WIDTH 100
-#define BOX_HEIGHT 40
 #define BOX_COLOUR Teal
 #define BOX_FRAME_COLOUR Silver
-#define BOX_OUTLINE_COLOUR Teal
-#define BOX_FONT_SIZE 18
+#define BOX_OUTLINE_COLOUR Black
+#define BUTTON_TEXT_COLOUR White
+
 
 image_handle_t backroundImage = NULL;
+image_handle_t logoImage = NULL;
 
 int drawBackround() {
     static int image_height;
@@ -36,27 +37,45 @@ int drawBackround() {
     }
 }
 
+int drawLogo(coord_t pos) {
+    static int image_height;
+    static int image_width;
+
+    if (logoImage == NULL) {
+        logoImage = tumDrawLoadImage(LOGO_FILENAME);
+    }
+
+    if ((image_height = tumDrawGetLoadedImageHeight(logoImage)) != -1) {
+        image_width = tumDrawGetLoadedImageWidth(logoImage);
+        tumDrawLoadedImage(logoImage, pos.x - image_width/2, pos.y - image_height/2);
+        return 1;
+
+    } else { 
+        printf("Failed to get size of image '%s', does it exist?\n",
+                BACKGROUND_FILENAME);
+        return 0;
+    }
+}
+
 int drawButton(coord_t pos, char *str) {
 
     static int text_width;
     font_handle_t cur_font = tumFontGetCurFontHandle();
 
     // Draw outlines
-    tumDrawFilledBox(pos.x , pos.y + 1, BOX_WIDTH, BOX_HEIGHT, BOX_OUTLINE_COLOUR);
-    tumDrawFilledBox(pos.x + 1, pos.y, BOX_WIDTH, BOX_HEIGHT, BOX_OUTLINE_COLOUR);
-    tumDrawFilledBox(pos.x + 1, pos.y + 1, BOX_WIDTH, BOX_HEIGHT, BOX_OUTLINE_COLOUR);
+    tumDrawFilledBox(pos.x , pos.y, BOX_WIDTH, BOX_HEIGHT + 3, BOX_OUTLINE_COLOUR);
     
     // Draw Box with Frame
-    tumDrawFilledBox(pos.x, pos.y, BOX_WIDTH, BOX_HEIGHT, BOX_FRAME_COLOUR);
-    tumDrawFilledBox(pos.x + 3, pos.y + 3, BOX_WIDTH - 6, BOX_HEIGHT - 6, BOX_COLOUR);
+    tumDrawFilledBox(pos.x + 2, pos.y + 2, BOX_WIDTH - 4, BOX_HEIGHT - 4, BOX_FRAME_COLOUR);
+    tumDrawFilledBox(pos.x + 4, pos.y + 4, BOX_WIDTH - 8, BOX_HEIGHT - 8, BOX_COLOUR);
 
     // Select Font
-    tumFontSelectFontFromName("SF Automaton.ttf");
+    tumFontSelectFontFromName(BUTTON_FONT);
 
     // Draw Text
     if (!tumGetTextSize((char *)str, &text_width, NULL)) {
         tumDrawText(str, pos.x + BOX_WIDTH/2 - text_width/2, 
-                         pos.y + BOX_HEIGHT/2 - BOX_FONT_SIZE/3 , BOX_FRAME_COLOUR);
+                         pos.y + BOX_HEIGHT/2 - BUTTON_FONT_SIZE/6*3 , BUTTON_TEXT_COLOUR);
         
     }           
 
