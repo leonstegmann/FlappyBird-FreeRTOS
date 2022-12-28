9ec50678872c22782caee094690851b6e0e9497e
Leon Stegmann VM <leon.stegmann@tum.de>
2022-12-28 17:55:16 +0100

    UPDATE working StateMachine

--- include/screens/menuScreen.h
+++ include/screens/menuScreen.h
@@ -1,6 +1,14 @@
 #ifndef __MENUSCREEN_H__
 #define __MENUSCREEN_H__
 
+/**
+ * @brief creates the FreeRTOS Task. Returns 0 on Succes, 1 on Error
+ */
 int createMenuTask();
+
+/* Enter and Exit functions for the State Machine */
+void deleteMenuTask();
+void enterMenuTask();
+void exitMenuTask();
 
 #endif // __MENUSCREEN_H__
--- src/stateMachine.c
+++ src/stateMachine.c
@@ -6,8 +6,8 @@
 #include "menuScreen.h"
 
 int initStateMachine(){
-    states_add( enterMenuTask, enterMenuTask, NULL, exitMenuTask, 0, "Menu_Screen");
+    states_add( (void*) createMenuTask, enterMenuTask, NULL, exitMenuTask, 0, "Menu_Screen");
     states_init();
     states_set_state(0);
-
+    return 0;
 }
