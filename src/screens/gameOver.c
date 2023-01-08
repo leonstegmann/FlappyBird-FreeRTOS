e7ed9c93a671da9d3368576a23241280c15bb3cc
Leon Stegmann VM <leon.stegmann@tum.de>
2023-01-08 17:41:50 +0100

    CLEAN UP removed unnecesarry Libraries

--- src/screens/gameOver.c
+++ src/screens/gameOver.c
@@ -1,16 +1,13 @@
 /* FreeRTOS includes  */
 #include "FreeRTOS.h"
-#include "semphr.h" // for buttons.lock
-#include"task.h" // for xTaskGetTickCount()
+#include "semphr.h" // for Semaphore take and give
+#include"task.h" // for Taskhandle_t
 #include <SDL2/SDL_scancode.h>  // Defines keyboard scancodes
 
-/* TUM_Library includes  */
-#include "TUM_FreeRTOS_Utils.h" //for tumFUtilPrintTaskStateList
-
 /* Project includes  */
-#include "main.h"
-#include "buttons.h"
-#include "draw.h"
+#include "main.h" // for Semaphore drawSignal
+#include "buttons.h" // for checkButton()
+#include "draw.h"   // for drawButtons() and drawGameOver()
 #include "states.h" //for set_stages()
 
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
